0ebef4f2c0e260b4b9d983ded82e8dec5f44da66
JailsonSantos <jailson.ads007@gmail.com>
2022-11-24 17:37:01 -0300

    Versão 1.0 da aplicação

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export function Button({ styles }: any) {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
       Get Started
     </button>
   )
--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,5 +1,16 @@
+import styles from '../styles'
+import { clients } from '../constants'
+
 export function Clients() {
   return (
-    <h1>Clients</h1>
+    <div className={`${styles.flexCenter} my4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
